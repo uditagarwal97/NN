c7d73bc8442cfe28adf4f2be238f42e559caf63f
temilola <malkyli.abik@gmail.com>
2021-11-16 22:22:08 -0800

    - Clean-up

--- src/main/java/ece/cpen502/Robot/RobotStates.java
+++ src/main/java/ece/cpen502/Robot/RobotStates.java
@@ -1,7 +1,6 @@
 package ece.cpen502.Robot;
 
 public class RobotStates {
-
     // X and Y coordinates of our robot
     int[] x = new int[8]; //x-position of the RL robot
     int[] y = new int[6]; //y-position of the RL robot
@@ -9,37 +8,39 @@ public class RobotStates {
     int[] bearing_angle = new int[4]; //Absolute Bearing Angle between RL robot and Enemy Robot
 
     // X and Y coordinates of our adversary
-   // int x2 = 0, y2 = 0;
+    int x1 = 0, y1 = 0;
+
+    // X and Y coordinates of our adversary
+     int x2 = 0, y2 = 0;
 
     // To determine whether we have sufficient firepower or not.
-    //Boolean IsFirePowerLeft = Boolean.TRUE;
+    Boolean IsFirePowerLeft = Boolean.TRUE;
 
-//    void SetState(double _x1, double _y1, double _x2, double _y2, double FirePower)
-//    {
-//        x1 = QuantizeCoordinate(_x1);
-//        x2 = QuantizeCoordinate(_x2);
-//        y1 = QuantizeCoordinate(_y1);
-//        y2 = QuantizeCoordinate(_y2);
-//
-//        IsFirePowerLeft = FirePower > 1.0;
-//    }
-//
-//    int QuantizeCoordinate(double val)
-//    {
-//        return (int) (val / 80);
-//    }
-//
-//    int IsFirePowerGreaterThanZero()
-//    {
-//        int retval = IsFirePowerLeft ? 1 : 0;
-//        return retval;
-//    }
-//
-//    void UpdateMyState(double _x1, double _y1)
-//    {
-//        x1 = QuantizeCoordinate(_x1);
-//        y1 = QuantizeCoordinate(_y1);
-//    }
+    void SetState(double _x1, double _y1, double _x2, double _y2, double FirePower)
+    {
+        x1 = QuantizeCoordinate(_x1);
+        x2 = QuantizeCoordinate(_x2);
+        y1 = QuantizeCoordinate(_y1);
+        y2 = QuantizeCoordinate(_y2);
 
+        IsFirePowerLeft = FirePower > 1.0;
+    }
+
+    int QuantizeCoordinate(double val)
+    {
+        return (int) (val / 80);
+    }
+
+    int IsFirePowerGreaterThanZero()
+    {
+        int retval = IsFirePowerLeft ? 1 : 0;
+        return retval;
+    }
+
+    void UpdateMyState(double _x1, double _y1)
+    {
+        x1 = QuantizeCoordinate(_x1);
+        y1 = QuantizeCoordinate(_y1);
+    }
 
 }
